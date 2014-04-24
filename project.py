ab80a0b125681b437976e7e20a71e63e30a60c5a
liliangwei <llwei89@gwmail.gwu.edu>
2014-04-23 21:07:19 -0400

    project day3

--- project.py
+++ project.py
@@ -4,6 +4,7 @@
 
 import numpy as np
 from scipy import integrate
+from sympy import Matrix
 from math import *
 import matplotlib.pyplot as plt
 
@@ -106,7 +107,7 @@ def definePanels(N,xp,yp):
         
     return panel
     
-NM,NF = 80,40                                     # Number of panels for each
+NM,NF = 8,4                                     # Number of panels for each
 Ntotal = NM + NF                                  # Total number of panels
 NA = Ntotal + 2                                   # Size of A matrix
 
@@ -134,8 +135,8 @@ plt.plot(np.append([p.xa for p in panelM],panelM[0].xa),\
         marker='D',markersize=6,color='r');
         
 # Create A matrix
-A = np.empty((NA,NA),dtype=float)
-
+A = np.zeros((NA,NA+1),dtype=float)
+B = np.zeros_like(A)
 # Merge all coefficients into one set
 # Do flap then main
 
@@ -192,8 +193,7 @@ for i in range(Ntotal):
                 else:
                     U1L = -(Y*log(R2/R1)+X*(TH2-TH1)-X2*(TH2-TH1))/(6.28319*X2)
                     U2L = (Y*log(R2/R1) + X*(TH2-TH1))/(6.28319*X2)
-                    W1L = -((X2-Y*(TH2-TH1))-X*log(R1/R2) \
-                            +X2*log(R1/R2))/(6.28319*X2)
+                    W1L = -((X2-Y*(TH2-TH1)) - X*log(R1/R2) + X2*log(R1/R2))/(6.28319*X2)
                     W2L = ((X2 - Y*(TH2-TH1))-X*log(R1/R2))/(6.28319*X2)
                        
                 
@@ -204,16 +204,16 @@ for i in range(Ntotal):
                 W2 = -U2L*sin(-TH[j]) + W2L*cos(-TH[j])
 
             # Compute the coefficients of gamma in the influence matrix
-                if (j==0) or (j==NF):
+                if ((j==0) or (j==NF)):
                     A[i,0] = -U1*sin(TH[i]) + W1*cos(TH[i])
                     HOLDA = -U2*sin(TH[i]) + W2*cos(TH[i])
                     B[i,0] = U1*cos(TH[i]) + W1*sin(TH[i])
                     HOLDB = U2*cos(TH[i]) + W2*sin(TH[i])
                 elif (j==NF-1):
-                    A[i,NM-1] = -U1*sin(TH[i]) + W1*cos(TH[i]) + HOLDA
-                    A[i,NM] = -U2*sin(TH[i]) + W2*cos(TH[i])
-                    B[i,NM-1] = U1*cos(TH[i]) + W1*sin(TH[i]) + HOLDB
-                    B[i,NM] + U2*cos(TH[i]) + W2*sin(TH[i])
+                    A[i,NF-1] = -U1*sin(TH[i]) + W1*cos(TH[i]) + HOLDA
+                    A[i,NF] = -U2*sin(TH[i]) + W2*cos(TH[i])
+                    B[i,NF-1] = U1*cos(TH[i]) + W1*sin(TH[i]) + HOLDB
+                    B[i,NF] = U2*cos(TH[i]) + W2*sin(TH[i])
                 else:
                     A[i,j] = -U1*sin(TH[i]) + W1*cos(TH[i]) + HOLDA
                     HOLDA = -U2*sin(TH[i]) + W2*cos(TH[i])
@@ -235,12 +235,9 @@ for i in range(Ntotal):
                 YT = CO2[i] - panelM[j-NF-1].ya
                 X2T = panelM[j-NF-1].xb-panelM[j-NF-1].xa
                 Y2T = panelM[j-NF-1].yb-panelM[j-NF-1].ya
-                
-                X = XT*cos(TH[j-1])+YT*sin(TH[j-1])
-                Y = -XT*cos(TH[j-1])+YT*cos(TH[j-1])
-                
-                X = XT*cos(TH[j-1]) + YT*sin(TH[j-1])
-                Y = -XT*sin(TH[j-1]) + YT*cos(TH[j-1])
+                print i,j
+                X = XT*cos(TH[j-1]) + YT*sin(TH[j-1])   # collocation point
+                Y = -XT*sin(TH[j-1]) + YT*cos(TH[j-1])  # collocation point
                 X1 = 0
                 Y1 = 0
                 X2 = X2T*cos(TH[j-1]) + Y2T*sin(TH[j-1])
@@ -264,12 +261,13 @@ for i in range(Ntotal):
                     U2L = 0.5*X/X2
                     W1L = -0.15916
                     W2L = 0.15916
+                    
                 else:
                     U1L = -(Y*log(R2/R1)+X*(TH2-TH1)-X2*(TH2-TH1))/(6.28319*X2)
                     U2L = (Y*log(R2/R1) + X*(TH2-TH1))/(6.28319*X2)
                     W1L = -((X2-Y*(TH2-TH1))-X*log(R1/R2) + X2*log(R1/R2))/(6.28310*X2)
                     W2L = ((X2-Y*(TH2-TH1))-X*log(R1/R2))/(6.28319*X2)
-                
+                    
         # Transferom the local velocities into global velocity function
                 U1 = U1L*cos(-TH[j-1]) + W1L*sin(-TH[j-1])
                 U2 = U2L*cos(-TH[j-1]) + W2L*sin(-TH[j-1])
@@ -292,11 +290,242 @@ for i in range(Ntotal):
                     HOLDA = -U2*sin(TH[i]) + W2*cos(TH[i])
                     B[i,j] = U1*cos(TH[i]) + W1*sin(TH[i]) + HOLDB
                     HOLDB = U2*cos(TH[i]) + W2*sin(TH[i])
-            else:
-                A[i,NA] = cos(AL) * sin(TH[i]) -sin(AL)*cos(TH[i])
+         
+        A[i,NA] = cos(AL) * sin(TH[i]) -sin(AL)*cos(TH[i])
     else:
     # We are dealing with the collocation point on the main
-    
+        for j in range(Ntotal+1):
+            if (j<NF):
+        # CO point i is on main, CO point j is on flap
         
+        # We find the influence coefficient for a specific collocation 
+        # point, i, on each of panels, j. Then we move to the n collocation point.
         
+        # Convert collocation point to local panel coordinates
+                XT = CO1[i] - panelF[j].xa
+                YT = CO2[i] - panelF[j].ya
+                X2T = panelF[j].xb - panelF[j].xa
+                Y2T = panelF[j].yb - panelF[j].ya
+                
+                X = XT*cos(TH[j]) + YT*sin(TH[j])     # collocation point
+                Y = -XT*sin(TH[j]) + YT*cos(TH[j])    # collocation point
+                X1 = 0
+                Y1 = 0
+                X2 = X2T*cos(TH[j]) + Y2T*sin(TH[j])
+                Y2 = 0
+                
+        # find the length of r1, r2, theta1 and theta2
+                R1 = sqrt((X-X1)**2 + (Y-Y1)**2)      # length from panel point 1 collocation point , panel coords
+                R2 = sqrt((X-X2)**2 + (Y-Y2)**2)      # length from panel point 2 collocation point, in panel coords
+                
+                TH1 = atan2(Y-Y1,X-X1)
+                TH2 = atan2(Y-Y2,X-X2)
+                
+                if (i==j):
+                    Y = 0
+                    TH1 = 0
+                
+        # Compute velocity componets as function of Gamma1 and Gamma2
+        # Velocity of panel j due to collocation point 1
+                if(i==j):
+                    U1L = -0.5*(X-X2)/X2
+                    U2L = 0.5*X/X2
+                    W1L = -0.15916
+                    W2L = 0.15916
+                else:
+                    U1L = -(Y*log(R2/R1)+X*(TH2-TH1)-X2*(TH2-TH1))/(6.28319*X2)
+                    U2L = (Y*log(R2/R1) + X*(TH2-TH1))/(6.28319*X2)
+                    W1L = -((X2-Y*(TH2-TH1)) - X*log(R1/R2) + X2*log(R1/R2))/(6.28319*X2)
+                    W2L = ((X2 - Y*(TH2-TH1))-X*log(R1/R2))/(6.28319*X2)
+        # Transform the local velocities into global velocity functions
+                
+                U1 = U1L*cos(-TH[j]) + W1L*sin(-TH[j])
+                U2 = U2L*cos(-TH[j]) + W2L*sin(-TH[j])
+                W1 = -U1L*sin(-TH[j]) + W1L*cos(-TH[j])
+                W2 = -U2L*sin(-TH[j]) + W2L*cos(-TH[j])
+                
+                if (j==0):
+                    A[i,0] = -U1*sin(TH[i]) + W1*cos(TH[i])
+                    HOLDA = -U2*sin(TH[i]) + W2*cos(TH[i])
+                    B[i,0] = U1*cos(TH[i]) + W2*sin(TH[i])
+                    HOLDB = U2*cos(TH[i]) + W2*sin(TH[i])
+                elif(j==NF-1):
+                    A[i,NF-1] = -U1*sin(TH[i]) + W1*cos(TH[i]) + HOLDA
+                    A[i,NF] = -U2*sin(TH[i]) + W2*cos(TH[i])
+                    B[i,NF-1] = U1*cos(TH[i]) + W1*sin(TH[i]) + HOLDB
+                    B[i,NF] = U2*cos(TH[i]) * W2*sin(TH[i])
+                else:
+                    A[i,j] = -U1*sin(TH[i]) + W1*cos(TH[i]) + HOLDA
+                    HOLDA = -U2*sin(TH[i]) + W2*cos(TH[i])
+                    B[i,j] = U1*cos(TH[i]) + W1*sin(TH[i]) + HOLDB
+                    HOLDB = U2*cos(TH[i]) + W2*sin(TH[i])
+            
+            elif (j>NF):
+            
+            # collocation point i is on main, collocation point j is on main
+            
+            # we find the influence coefficient for a specific collocation
+            # point, i, on each of panel, j. Then we move on to the next collocation point
+            
+            # since there are two separate airfolds, point j = NF
+            # should not be solved for (this is the 2nd edge of the last panel)
+            # During the for loop, any j after
+            # this should be j-1, to reference the correct panel, except when referring to matrix A
+            
+            # Convert collocation point To local panel coordinates
+                XT = CO1[i] - panelM[j-NF-1].xa
+                YT = CO2[i] - panelM[j-NF-1].ya
+                X2T = panelM[j-NF-1].xb - panelM[j-NF-1].xa
+                Y2T = panelM[j-NF-1].yb - panelM[j-NF-1].ya
+                
+                X = XT*cos(TH[j-1]) + YT*sin(TH[j-1])
+                Y = -XT*sin(TH[j-1]) + YT*cos(TH[j-1])
+                X1 = 0
+                Y1 = 0
+                X2 = X2T*cos(TH[j-1]) + Y2T*sin(TH[j-1])
+                Y2 = 0
+            # Find the length of r1, r2, theta1, and theta2
+                R1 = sqrt((X-X1)**2 + (Y-Y1)**2)  # length from panel point 1 to collocation point, in panel coords
+                R2 = sqrt((X-X2)**2 + (Y-Y2)**2)  # length from panel point 2 to collocation point, in panel coords
+                
+                TH1 = atan2(Y-Y1,X-X1)
+                TH2 = atan2(Y-Y2,X-X2)
+                
+                if (i==j-1):
+                    Y = 0
+                    TH1 = 0
+                
+            # Compute velocity components as functions of Gamma1 and Gamma2
+            # Velocity of panel j due to collocation point i 
+                if (i==j-1):
+                    U1L = -0.5*(X-X2)/X2
+                    U2L = 0.5*X/X2
+                    W1L = -0.15916
+                    W2L = 0.15916
+                else:
+                    U1L = -(Y*log(R2/R1) + X*(TH2-TH1) - X2*(TH2-TH1))/(6.28319*X2)
+                    U2L = (Y*log(R2/R1) + X*(TH2-TH1))/(6.28319*X2)
+                    W1L = -((X2-Y*(TH2-TH1)) - X*log(R1/R2) + X2*log(R1/R2))/(6.28319*X2)
+                    W2L = ((X2-Y*(TH2-TH1)) - X*log(R1/R2))/(6.28319*X2)
+                
+            # Transform the local velocities into global velocity functions
+                U1 = U1L*cos(-TH[j-1]) + W1L*sin(-TH[j-1])
+                U2 = U2L*cos(-TH[j-1]) + W2L*sin(-TH[j-1])
+                W1 = -U1L*sin(-TH[j-1]) + W1L*cos(-TH[j-1])
+                W2 = -U2L*sin(-TH[j-1]) + W2L*cos(-TH[j-1])
+                
+            # Compute the coefficients of gamma in the influence matrix
+                if (j == NF+1):
+                    A[i,NF+1] = -U1*sin(TH[i]) + W1*cos(TH[i])
+                    HOLDA = -U2*sin(TH[i]) + W2*cos(TH[i])
+                    B[i,NF+1] = U1*cos(TH[i]) + W1*sin(TH[i])
+                    HOLDB = U2*cos(TH[i]) + W2*sin(TH[i])
+                elif (j==Ntotal):
+                    A[i,Ntotal] = -U1*sin(TH[i]) + W1*cos(TH[i]) + HOLDA
+                    A[i,NA-1] = -U2*sin(TH[i]) + W2*cos(TH[i])
+                    B[i,Ntotal] = U1*cos(TH[i]) + W1*sin(TH[i]) + HOLDB
+                    B[i,NA-1] = U2*cos(TH[i]) + W2*sin(TH[i])
+                else:
+                    A[i,j] = -U1*sin(TH[i]) + W1*cos(TH[i]) + HOLDA
+                    HOLDA = -U2*sin(TH[i]) + W2*cos(TH[i])
+                    B[i,j] = U1*cos(TH[i]) + W1*sin(TH[i]) + HOLDB
+                    HOLDB = U2*cos(TH[i]) + W2*sin(TH[i])
+                
+        A[i,NA] = cos(AL) * sin(TH[i]) - sin(AL)* cos(TH[i])
+                    
 
+        # Add both kutta conditions. Be careful of where the ones are
+        # matrix columns NF and Ntotal+1 are the last edges of the airfold
+
+#Flap
+A[NA-2,0] = 1
+A[NA-2,NF] = 1
+#Main
+A[NA-1,NF+1] = 1
+A[NA-1,NA-1] = 1
+
+# Linear solver
+def ToReducedRowEchelonForm(M):
+    #if not M : return
+    lead = 0
+    rowCount = len(M)
+    columnCount = len(M[0])
+    for r in range(rowCount):
+        if lead >= columnCount:
+            return
+        i=r
+        while M[i][lead] == 0:
+            i+= 1
+            if i==rowCount:
+                i = r
+                lead += 1
+                if columnCount == lead:
+                    return
+        M[i],M[r] = M[r],M[i]
+        lv = M[r][lead]
+        M[r] = [mrx/lv for mrx in M[r]]
+        for i in range(rowCount):
+            if i != r:
+                lv = M[i][lead]
+                M[i] = [iv - lv*rv for rv,iv in zip(M[r],M[i])]
+        lead += 1
+   
+    return M
+
+
+R = ToReducedRowEchelonForm(A)
+
+G = R[:,NA]
+
+CL = 0 
+CP = np.zeros(Ntotal,dtype=float)
+# calculate variables of interest
+for i in range(Ntotal):
+    VEL = 0
+    for j in range(NA):
+        VEL = VEL + B[i,j]*G[j]
+    
+    V = VEL + cos(AL)*cos(TH[i]) + sin(AL) * sin(TH[i])
+    CP[i] = 1 - V**2
+
+CP = np.transpose(CP)
+
+for i in range(Ntotal):
+    if (i<NF):
+        panelF[i].Cp = CP[i]
+    else:
+        panelM[i-NF].Cp = CP[i]
+
+    
+#    CL = CL + -1.0*CP[i]*(cos(AL)*cos(TH[i]) + sin(A)* sin(TH[i]))*DL[i]
+#print [p.Cp for p in panelF]
+
+# Plot pressure coefficients
+valX,valY = 0.2,0.4
+xmin,xmax = min([p.xa for p in panelM]),max([p.xa for p in panelF])
+Cpmin,Cpmax = min([p.Cp for p in panelF]),max([p.Cp for p in panelM])
+xStart,xEnd = xmin-valX*(xmax-xmin),xmax+valX*(xmax-xmin)
+yStart,yEnd = Cpmin-valY*(Cpmax-Cpmin),Cpmax+valY*(Cpmax-Cpmin)      
+plt.figure(figsize=(10,6))
+plt.grid(True)
+plt.xlabel('x',fontsize=16)
+plt.ylabel('$C_p$',fontsize=16)
+plt.plot([p.xc for p in panelM if p.loc=='extrados'],\
+		[p.Cp for p in panelM if p.loc=='extrados'],\
+		'ro-',linewidth=2)
+plt.plot([p.xc for p in panelM if p.loc=='intrados'],\
+		[p.Cp for p in panelM if p.loc=='intrados'],\
+		'bo-',linewidth=1)
+plt.plot([p.xc for p in panelF if p.loc=='extrados'],\
+		[p.Cp for p in panelF if p.loc=='extrados'],\
+		'ro-',linewidth=2)
+plt.plot([p.xc for p in panelF if p.loc=='intrados'],\
+		[p.Cp for p in panelF if p.loc=='intrados'],\
+		'bo-',linewidth=1)
+plt.legend(['extrados','intrados'],'best',prop={'size':14})
+plt.plot([p.xc for p in panelF],[p.Cp for p in panelF],'ro',linewidth=2)
+plt.xlim(xStart,xEnd)
+plt.ylim(yStart,yEnd)
+#plt.gca().invert_yaxis();
+
+#plt.show()
